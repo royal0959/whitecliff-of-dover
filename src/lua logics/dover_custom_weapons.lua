1a892c3143ff1f90b5267bd8a5e618e93572e739
royal0959 <70254342+royal0959@users.noreply.github.com>
2022-08-21 22:41:40 +0700

    fixes

--- src/lua logics/dover_custom_weapons.lua
+++ src/lua logics/dover_custom_weapons.lua
@@ -23,7 +23,7 @@ local PHD_EXPLOSIONS = {
 
 local PARRY_TIME = 0.8
 
-local SCAVENGER_EXPLOSION_BASE_DAMAGE = 300 -- actual damage is half of this
+local SCAVENGER_EXPLOSION_BASE_DAMAGE = 225
 
 local classIndices_Internal = {
 	[1] = "Scout",
